2d9c6dc449090b35845bde6d30674f84839241fe
amitgiri257 <127419759+amitgiri257@users.noreply.github.com>
2023-09-24 21:03:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+var name1 = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+var confirmPass = document.getElementById("confirm-password").value;
+var btn = document.getElementById("btn);
+
+btn.addEventListener("click",function(){
+	if(password===confirmPass){
+		sessionStorage.setItem(name1,email,password, JSON.stringify(name1,email,password));
+		alert("Sign up successful!")
+	}
+	else{
+		alert("Passwords do not match");
+	}
+})
